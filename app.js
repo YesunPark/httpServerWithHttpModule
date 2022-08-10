033d7eb24374739cdc2035aa8dfb62293fae9dd2
YesunPark <lynn08082@gmail.com>
2022-08-10 19:19:24 +0900

    Feat: 미션3,4 완성

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//mission 1 회원가입
 const users = [
   {
     id: 1,
@@ -13,7 +14,6 @@ const users = [
   },
 ];
 
-//mission 1
 const createUser = (req, res) => {
   const user = req.body.data; // 프론트에서 받아온 정보를 가져옵니다.
   const { id, name, email, password } = req.body.data;
@@ -21,13 +21,12 @@ const createUser = (req, res) => {
   //javaScript의 특징 중 하나 : key와 value 값이 똑같으면 안써도 됨...! 대박
   users.push({ id, name, email, password });
 
-  console.log("after: ", users);
-
-  res.json({ message: "USER_CREATED" });
+  res.status(201).json({ message: "USER_CREATED" });
   // express 덕분에 JSON.stringify 함수를 사용할 필요없이
   // response 객체의 json 메소드를 활용합니다.
 };
 
+//mission 2 게시글 등록
 const posts = [
   {
     id: 1,
@@ -41,11 +40,16 @@ const posts = [
     content: "Request/Response와 Stateless!!",
     userId: 1,
   },
+  {
+    id: 3,
+    title: "HTTP의 구조",
+    content: "구조를 배워보자",
+    userId: 2,
+  },
 ];
 
-//mission 2
 const createPost = (req, res) => {
-  const user = req.body.post;
+  //const user = req.body.post;
   const { id, title, content, userId } = req.body.post;
 
   posts.push({ id, title, content, userId });
@@ -55,4 +59,60 @@ const createPost = (req, res) => {
   res.json({ message: "postCreated" });
 };
 
-module.exports = { createUser, createPost };
+//mission 3 게시글목록 조회
+const PostsList = (req, res) => {
+  res.json({
+    data: [
+      {
+        userID: 1,
+        userName: "Rebekah Johnson",
+        postingId: 1,
+        postingTitle: "간단한 HTTP API 개발 시작!",
+        postingContent:
+          "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+      },
+      {
+        userID: 2,
+        userName: "Fabian Predovic",
+        postingId: 2,
+        postingTitle: "HTTP의 특성",
+        postingContent: "Request/Response와 Stateless!!",
+      },
+      {
+        userID: 3,
+        userName: "new user 1",
+        postingId: 3,
+        postingImageUrl: "내용 1",
+        postingContent: "sampleContent3",
+      },
+      {
+        userID: 4,
+        userName: "new user 2",
+        postingId: 4,
+        postingImageUrl: "내용 2",
+        postingContent: "sampleContent4",
+      },
+    ],
+  });
+};
+
+//mission 4 게시글 수정
+const updatePost = (req, res) => {
+  posts.forEach((el, i) => {
+    const user = req.body.data;
+
+    if (user["postingId"] == posts[i]["id"]) {
+      res.status(200).json({
+        data: {
+          userId: posts[i]["userId"],
+          userName: users[posts[i]["userId"] - 1]["name"],
+          postingId: posts[i]["id"],
+          postingTitle: posts[i]["title"],
+          postingContent: user.postingContent,
+        },
+      });
+    }
+  });
+};
+
+module.exports = { createUser, createPost, PostsList, updatePost };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const express = require("express");
 const { sendProducts } = require("./sendProducts");
-const { createUser, createPost } = require("./app");
+const { createUser, createPost, PostsList, updatePost } = require("./app");
 
 const app = express();
 app.use(express.json());
@@ -11,15 +11,16 @@ app.get("/ping", (req, res) => {
 });
 
 app.post("/signup", createUser);
-app.post("/post", createPost);
 app.post("/login", (req, res) => {
   res.json("login success");
 });
+app.post("/post", createPost);
 app.get("/products", sendProducts);
+app.get("/postsList", PostsList);
+app.patch("/updatePost", updatePost);
 
 const server = http.createServer(app);
 
 server.listen(8000, () => {
   console.log("server is listening on PORT 8000");
 });
-//주석추가
